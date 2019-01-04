beb3cacdf66a94849c576d0e20f8a3b878273ec3
junq <454099185@qq.com>
2019-01-04 19:18:56 +0800

    test

--- protein_test.py
+++ protein_test.py
@@ -10,7 +10,7 @@ import pandas as pd
 import tensorflow as tf
 from time import time
 
-os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+os.environ["CUDA_VISIBLE_DEVICES"] = '1'
 
 flags = tf.app.flags
 
@@ -23,14 +23,13 @@ flags.DEFINE_string('test_dataset_path',
 #                     'Folder containing dataset.')
 flags.DEFINE_string('save_path', './result/protein',
                     'Path to output submission file.')
-# flags.DEFINE_float('threshould', 0.19, 'The momentum value to use')
 
 FLAGS = flags.FLAGS
 
-_THRESHOULD = [0.1853, 0.1357, 0.1221, 0.2865, 0.2245, 0.1601, 0.1177, 
-               0.1733, 0.0081, 0.0005, 0.0005, 0.1473, 0.2457, 0.2321,
-               0.2389, 0.0013, 0.0429, 0.1421, 0.1245, 0.2277, 0.1625,
-               0.2141, 0.1061, 0.1745, 0.0417, 0.1541, 0.1345, 0.0029]
+_THRESHOULD = [0.0457, 0.0953, 0.0921, 0.1265, 0.0149, 0.3209, 0.1713, 
+               0.1061, 0.6000, 0.1421, 0.6000, 0.6000, 0.6000, 0.6000,
+               0.1665, 0.6000, 0.3017, 0.6000, 0.3249, 0.2305, 0.0717,
+               0.1269, 0.3593, 0.0737, 0.1681, 0.0653, 0.6000, 0.6000]
 
 def model_test():
   g = tf.Graph()
@@ -38,9 +37,9 @@ def model_test():
     od_graph_def = tf.GraphDef()
     with tf.gfile.FastGFile(os.path.join(FLAGS.save_path, 'model.pb'), 'rb') as f:
         od_graph_def.ParseFromString(f.read())
-        img_tensor, prediction, counts_prediction = tf.import_graph_def(
+        img_tensor, prediction = tf.import_graph_def(
                 od_graph_def,
-                return_elements=['ImageTensor:0', 'Prediction:0', 'CountsPrediction:0'])
+                return_elements=['ImageTensor:0', 'Prediction:0'])
     init_op = tf.global_variables_initializer()
     test_data = pd.read_csv(os.path.join(FLAGS.test_dataset_path, 'sample_submission.csv'))
     session_config = tf.ConfigProto(allow_soft_placement=True)
@@ -59,10 +58,9 @@ def model_test():
               [image_data_green, image_data_red, image_data_blue, image_data_yellow], axis=-1)
           image_data = (image_data / 255).astype(np.float32)
 
-          logits_np, counts_np = sess.run([prediction, counts_prediction], {img_tensor: image_data})
-          
+          logits_np = sess.run(prediction, {img_tensor: image_data})
+
           logits_np = logits_np[0]
-          counts_np = counts_np[0]
           predictions_id = list(np.where(logits_np > _THRESHOULD)[0])
           if predictions_id is None:
             max_id = np.argmax(logits_np)
--- test.py
+++ test.py
@@ -147,16 +147,17 @@ def model_test_ensemble():
 
 
 def logits_ensemble():
-  pred_a = pd.read_csv('./fine_tune/logits-803207.csv', sep=',', header=None).values
+  pred_a = pd.read_csv('./fine_tune/logits-887849.csv', sep=',', header=None).values
   pred_b = pd.read_csv('./fine_tune/logits-1850888.csv', sep=',', header=None).values
-  pred_c = pd.read_csv('./fine_tune/test_re.csv', sep=',', header=None).values
+  pred_c = pd.read_csv('./fine_tune/test_re0.csv', sep=',', header=None).values
+  pred_d = pd.read_csv('./fine_tune/test_re1.csv', sep=',', header=None).values
   pred_rows = []
   for i in range(4838):
-    pred_logits = (pred_a[i] + pred_b[i] + pred_c[i]) / 3
+    pred_logits = pred_a[i] + pred_b[i] + pred_c[i] + pred_d[i]
     pred = np.zeros([17], np.uint8)
     pred[np.argmax(pred_logits)] = 1
     pred_rows.append(pred)
-  np.savetxt('./result/lcz/submission-ensemble-28(803207+1850888+L).csv', pred_rows, delimiter=",", fmt='%s')
+  np.savetxt('./result/lcz/submission-29-ensemble(887849+1850888+L0+L1).csv', pred_rows, delimiter=",", fmt='%s')
 
 
 if __name__ == '__main__':
